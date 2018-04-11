ab5a7d32d7768a1c4de78792c9f708c68ec5b808
Michal Balazi <balazi.michal@gmail.com>
2018-04-11 09:30:29 +0200

    feat
    Filtering books into book shelves based on the shelf property (read, wantToRead, currentlyReading)

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -7,7 +7,7 @@ class ListBooks extends Component {
   render() {
     console.log(this.props.books)
     return (
-
+      //const books = this.props.books
 
       <div className="list-books">
         <div className="list-books-title">
@@ -19,35 +19,30 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-
-
                   {this.props.books.map((book) => {
-                    return (
-                    <li key={book.industryIdentifiers[0].identifier}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="none" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
+                    if(book.shelf === 'currentlyReading'){
+                      return (
+                        <li key={book.industryIdentifiers[0].identifier}>
+                          <div className="book">
+                            <div className="book-top">
+                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                              <div className="book-shelf-changer">
+                                <select>
+                                  <option value="none" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
                           </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors}</div>
-                      </div>
-                    </li>
-                    )
+                        </li>
+                      )
+                    }
                   })}
-
-
-
-
-
                 </ol>
               </div>
             </div>
@@ -56,6 +51,31 @@ class ListBooks extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
 
+                {this.props.books.map((book) => {
+                  if(book.shelf === 'wantToRead'){
+                    return (
+                      <li key={book.industryIdentifiers[0].identifier}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    )
+                  }
+                })}
+
                 </ol>
               </div>
             </div>
@@ -63,6 +83,31 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
+
+                {this.props.books.map((book) => {
+                  if(book.shelf === 'read'){
+                    return (
+                      <li key={book.industryIdentifiers[0].identifier}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
+                        </div>
+                      </li>
+                    )
+                  }
+                })}
 
                 </ol>
               </div>
