a8454da7dbfea2bf15029e1f5b6f3d81bf5c1b34
Michal Balazi <balazi.michal@gmail.com>
2018-04-11 08:44:50 +0200

    feat
    Implementing React router to navigate between the search page and list of books

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './SearchBooks.js'
 import ListBooks from './ListBooks.js'
+import { Route } from 'react-router-dom'
+
+
+
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books
+        }))
+      })
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks />
-        ) : (
-          <ListBooks />
-        )}
+          <Route exact path='/' render={() => (
+            <ListBooks books={this.state.books}/>
+          )} />
+          <Route path='/search' render={() => (
+            <SearchBooks />
+          )} />
       </div>
     )
   }
