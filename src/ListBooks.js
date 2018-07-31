9118c1ee4f059ecfe6ce36abc70edbbe4df53e8e
Michal Balazi <balazi.michal@gmail.com>
2018-07-31 11:26:49 +0200

    fix - change shelf prop added to the ListBooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import SingleBook from './SingleBook.js'
 import BookShelf from './BookShelf.js'
 
 
@@ -15,9 +14,9 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <BookShelf books={this.props.books} shelf={'currentlyReading'} />
-            <BookShelf books={this.props.books} shelf={'wantToRead'} />
-            <BookShelf books={this.props.books} shelf={'read'} />
+            <BookShelf books={this.props.books} changeShelf={this.props.changeShelf} shelf={'currentlyReading'} />
+            <BookShelf books={this.props.books} changeShelf={this.props.changeShelf} shelf={'wantToRead'} />
+            <BookShelf books={this.props.books} changeShelf={this.props.changeShelf} shelf={'read'} />
           </div>
         </div>
         <div className="open-search">
