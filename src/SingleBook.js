5439abd90c0fe65b5358fbc8d2545b72b59126d4
Michal Balazi <balazi.michal@gmail.com>
2018-08-03 13:08:29 +0200

    fix - removed "none" value from disabled option

--- src/SingleBook.js
+++ src/SingleBook.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
 
 class SingleBook extends Component {
-
-
-
-
-
   render(){
-
     return(
-
 
       <li>
         <div className="book">
@@ -19,7 +12,7 @@ class SingleBook extends Component {
 
             <div className="book-shelf-changer">
               <select onChange={(event) => this.props.changeShelf(this.props.book, event.target.value)} value={this.props.book.shelf || 'none'}>
-                <option value="none" disabled>Move to...</option>
+                <option value="" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
@@ -31,6 +24,7 @@ class SingleBook extends Component {
           <div className="book-authors">{this.props.book.authors}</div>
         </div>
       </li>
+      
     )
   }
 }
